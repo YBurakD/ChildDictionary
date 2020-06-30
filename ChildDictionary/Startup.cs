cde49ab38e3fddc402b291faae1016332b5f0da1
Yunus Burak Durmuş <y.burak.durmus@gmail.com>
2020-06-30 19:12:03 +0300

    Database Connection

--- ChildDictionary/Startup.cs
+++ ChildDictionary/Startup.cs
@@ -27,10 +27,10 @@ namespace ChildDictionary
         {
             services.AddControllersWithViews();
 
-           services.AddDbContext<ChildDictionaryContext>(options =>
-                  options.UseSqlServer(Configuration.GetConnectionString("ChildDictionaryContext")));
            //services.AddDbContext<ChildDictionaryContext>(options =>
-             //   options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+             //     options.UseSqlServer(Configuration.GetConnectionString("ChildDictionaryContext")));
+           services.AddDbContext<ChildDictionaryContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
