ef29524b0dfe774cb020e00aee765ec7dbf0bf24
ituitis-durmusy18 <durmusy18@itu.edu.tr>
2020-06-30 14:47:29 +0300

    güzelladım az bişeler

--- ChildDictionary/wwwroot/js/site.js
+++ ChildDictionary/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#searchbutton").click(function (e) {
+	e.preventDefault();
+	var elem = document.getElementById("reading");
+	var elem2 = document.getElementById("family");
+	var elem3 = document.getElementById("plane");
+	var elem4 = document.getElementById("searchbox");
+	var elem5 = document.getElementById("searchbutton");
+	var elem6 = document.getElementById("language");
+	var pos = 0;
+	var op = 200;
+	var id = setInterval(frame, 3);
+	function frame() {
+		if (op == 0) {
+			clearInterval(id);
+		} else {
+			pos++;
+			op--;
+			elem.style.opacity = op / 200;
+			elem2.style.opacity = op / 200;
+			elem3.style.top = -pos + 'px';
+			elem3.style.left = -pos + 'px';
+			elem3.style.opacity = op / 200;
+			elem4.style.top = -pos + 'px';
+			elem5.style.top = -pos + 'px';
+			elem6.style.top = -pos + 'px';
+			
+		}
+		$('#myform').submit();
+	}
+	elem.hide;
+	elem2.hide;
+	elem3.hide;
+	
+});
